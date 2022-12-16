5b6862e1567a1bf68df7bcfd4aa8c973545961d2
Ezgi Pektaş <ezgiipektas@gmail.com>
2022-12-16 18:24:59 +0300

    2.0

---
+++ Core_Project/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Core_Project/Controllers/DefaultController.cs
+++ Core_Project/Controllers/DefaultController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Core_Project.Controllers
 {
@@ -14,6 +18,20 @@ namespace Core_Project.Controllers
         }
         public PartialViewResult NavbarPartial()
         {
+            return PartialView();
+        }
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SendMessage(Message p)
+        {
+            MessageManager mm = new MessageManager(new EfMessageDal());
+            p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.Status = true;
+            mm.TAdd(p);
             return PartialView();
         }
     }
