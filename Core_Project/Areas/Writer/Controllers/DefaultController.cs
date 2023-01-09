3f310108bccb6aa178221d572b2e5f3392e0b6c9
Ezgi Pektaş <ezgiipektas@gmail.com>
2023-01-10 01:21:16 +0300

    9.01

--- Core_Project/Areas/Writer/Controllers/DefaultController.cs
+++ Core_Project/Areas/Writer/Controllers/DefaultController.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Areas.Writer.Controllers
 {
+    [Area("Writer")]
+    [Authorize]
     public class DefaultController : Controller
     {
-        [Area("Writer")]
-        [Authorize ]
+        AnnouncementManager anm = new AnnouncementManager(new EfAnnouncementDal());
+        
         public IActionResult Index()
         {
-            return View();
+            var values = anm.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AnnouncementDetails(int id)
+        {
+            var values = anm.TGetByID(id);
+            return View(values);
+
         }
     }
 }
